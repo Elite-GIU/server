2df17595e58ffdd92af5e59674cead796bbf6bb7
momenashrafff <momenashraf742@gmail.com>
2024-11-19 23:41:41 +0200

    created jwt authentication, guards, and strategy and login/register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './modules/user/user.module';
-
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,10 @@
+import { ConfigService } from '@nestjs/config';
+
+export const JwtConfig = (configService: ConfigService) => ({
+  secret: configService.get<string>('JWT_SECRET'),
+  signOptions: { expiresIn: '1h' },
+});
+
+
+
+
