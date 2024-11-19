e9e4b3e3493ee7d3335383260f15752c14642bb8
momenashrafff <momenashraf742@gmail.com>
2024-11-19 23:36:32 +0200

    created user module, finished create user, and find by email

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../../database/schemas/user.schema';
+import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
-  async createUser(name: string, email: string, password: string): Promise<User> {
-    const newUser = new this.userModel({ name, email, password });
-    return newUser.save();
+  // Method to create a new user
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new this.userModel(createUserDto);
+    return newUser.save(); 
   }
 
-  async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
+  // Method to find a user by email
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
   }
 }
