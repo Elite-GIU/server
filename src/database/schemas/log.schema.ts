30fdc2cb2e3e198a75f8c581542a6ed25538c735
MohammadMoharram <95143285+MohamedHossam2004@users.noreply.github.com>
2024-11-21 21:59:32 +0200

    feat: add schema for logs

---
+++ src/database/schemas/log.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema({ timestamps: { createdAt: 'timestamp', updatedAt: false } })
+export class Log extends Document {
+  @Prop({ type: Types.ObjectId, required: false, ref: 'User' })
+  user_id?: Types.ObjectId; 
+
+  @Prop({ required: true })
+  event: string; 
+
+  @Prop({ required: true })
+  timestamp: Date; 
+
+  @Prop({ required: true })
+  status: number;
+
+  @Prop({ required: true, enum: ['auth', 'general'] })
+  type: string;
+}
+
+export const LogSchema = SchemaFactory.createForClass(Log);
