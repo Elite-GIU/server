b3eb50dbdaf549c24b250ebffce277fabde0139f
Sarah <sarah.elfeel@gmail.com>
2024-11-28 02:05:25 +0200

    feat: add status to studentCourse schema and adjust typecasting in module service and handle injection

--- src/modules/module/module.module.ts
+++ src/modules/module/module.module.ts
@@ -4,11 +4,13 @@ import { ModuleService } from './module.service';
 import { ModuleEntity, ModuleSchema } from '../../database/schemas/module.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Questionbank, QuestionbankSchema } from 'src/database/schemas/questionbank.schema';
+import { StudentCourse, StudentCourseSchema } from 'src/database/schemas/studentCourse.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ModuleEntity.name, schema: ModuleSchema }]),
     MongooseModule.forFeature([{ name: Questionbank.name, schema: QuestionbankSchema }]),
+    MongooseModule.forFeature([{ name: StudentCourse.name, schema: StudentCourseSchema }]),
   ],
   controllers: [ModuleController],
   providers: [ModuleService],
--- src/modules/module/module.service.ts
+++ src/modules/module/module.service.ts
@@ -1,26 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ModuleEntity } from '../../database/schemas/module.schema';
 import { Questionbank } from '../../database/schemas/questionbank.schema';
+import { StudentCourse } from 'src/database/schemas/studentCourse.schema';
 @Injectable()
 export class ModuleService {
     constructor(
         @InjectModel(ModuleEntity.name) private readonly moduleModel: Model<ModuleEntity>,
         @InjectModel(Questionbank.name) private readonly questionbankModel: Model<Questionbank>,
+        @InjectModel(StudentCourse.name) private readonly studentCourseModel: Model<StudentCourse>
     ) {}
 
    async getModuleContent(userId: string, courseId: string, moduleId: string) {
-        const enrollment = await this.moduleModel.findOne({
-          user_id: userId,
-          course_id: courseId,
+        const enrollment = await this.studentCourseModel.findOne({
+          user_id: new Types.ObjectId(userId),
+          course_id: new Types.ObjectId(courseId),
         });
         if (!enrollment) throw new NotFoundException('Course enrollment not found');
     
         const module = await this.moduleModel.findById(moduleId);
         if (!module) throw new NotFoundException('Module not found');
     
-        const quiz = await this.questionbankModel.exists({ module_id: moduleId });
+        const quiz = await this.questionbankModel.exists({ module_id: new Types.ObjectId(moduleId) });
     
         return {
           ...module.toObject(),
