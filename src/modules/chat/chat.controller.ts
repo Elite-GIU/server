2b55ba856fb6f4e86780a393784866631f3bd8cb
Powerscore <alaa.ashraf.helmy@gmail.com>
2024-12-04 00:23:12 +0200

    Started thread endpoints

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -12,6 +12,9 @@ import { RoomMessageDto } from './dto/RoomMessageDto';
 import { ExistParam } from 'src/common/decorators/existParam.decorator';
 import { CheckExistValidatorPipe } from 'src/common/pipes/check-exist-validator.pipe';
 import { GetUser } from 'src/common/decorators/getUser.decorator';
+import { ThreadDto } from './dto/ThreadDto';
+import { ThreadMessageDto } from './dto/ThreadMessageDto';
+import { ThreadMessageReplyDto } from './dto/ThreadMessageReplyDto';
 @Controller('chat')
 @ApiTags('Chat')
 @UseGuards(JwtAuthGuard)
@@ -118,5 +121,193 @@ export class ChatController {
     );
   }
 
-  // GET /forums/courses/:courseId - Get messages of a thread
+  //---------------------------------- FORUMS ----------------------------------\\
+
+  // GET /forums/courses/:courseId - Get threads of a course
+  @Get('forums/courses/:id')
+  @ApiParam({ name: 'id', required: true, description: 'Course ID' })
+  @ApiOperation({ summary: 'Get threads of a course' })
+  @ApiResponse({ status: 200, description: 'Threads fetched successfully' })
+  @ApiResponse({ status: 404, description: 'Thread Not Found' })
+  async getThreadMessages(
+    @ExistParam({ idKey: 'id', modelName: 'Course' }, CheckExistValidatorPipe)
+    course: {
+      id: string;
+      modelName: string;
+    },
+  ) {
+    return this.chatService.getCourseThreads(course.id);
+  }
+  // GET chat/forums/course:id/threads/thread:id - Get specific thread messages
+  @Get('forums/courses/:courseId/threads/:threadId')
+  @ApiParam({ name: 'courseId', required: true, description: 'Course ID' })
+  @ApiParam({ name: 'threadId', required: true, description: 'Thread ID' })
+  @ApiOperation({ summary: 'Get specific thread messages' })
+  @ApiResponse({
+    status: 200,
+    description: 'Thread messages fetched successfully',
+  })
+  @ApiResponse({ status: 404, description: 'Thread not found' })
+  async getThreadReplies(
+    @ExistParam(
+      { idKey: 'courseId', modelName: 'Course' },
+      CheckExistValidatorPipe,
+    )
+    course: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'threadId', modelName: 'Thread' },
+      CheckExistValidatorPipe,
+    )
+    thread: {
+      id: string;
+      modelName: string;
+    },
+  ) {
+    return this.chatService.getThreadMessages(course.id, thread.id);
+  }
+
+  // GET chat/forums/course:id/threads/thread:id/messages/message:id - Get message replies
+  @Get('forums/courses/:courseId/threads/:threadId/messages/:messageId')
+  @ApiParam({ name: 'courseId', required: true, description: 'Course ID' })
+  @ApiParam({ name: 'threadId', required: true, description: 'Thread ID' })
+  @ApiParam({ name: 'messageId', required: true, description: 'Message ID' })
+  @ApiOperation({ summary: 'Get thread message replies' })
+  @ApiResponse({
+    status: 200,
+    description: 'Message replies fetched successfully',
+  })
+  @ApiResponse({ status: 404, description: 'Reply not found' })
+  async getMessageReplies(
+    @ExistParam(
+      { idKey: 'courseId', modelName: 'Course' },
+      CheckExistValidatorPipe,
+    )
+    course: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'threadId', modelName: 'Thread' },
+      CheckExistValidatorPipe,
+    )
+    thread: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'messageId', modelName: 'Message' },
+      CheckExistValidatorPipe,
+    )
+    message: {
+      id: string;
+      modelName: string;
+    },
+  ) {
+    return this.chatService.getThreadMessageReplies(
+      course.id,
+      thread.id,
+      message.id,
+    );
+  }
+
+  //POST chat/forums/courses/course:id - Post new thread
+  @Post('forums/courses/:id')
+  @ApiParam({ name: 'id', required: true, description: 'Course ID' })
+  @ApiOperation({ summary: 'Post a new thread' })
+  @ApiResponse({ status: 201, description: 'Thread created successfully' })
+  @ApiResponse({ status: 404, description: 'Thread not found' })
+  async postThread(
+    @GetUser('userId') userId: string,
+    @ExistParam({ idKey: 'id', modelName: 'Course' }, CheckExistValidatorPipe)
+    course: {
+      id: string;
+      modelName: string;
+    },
+    @Body() threadData: ThreadDto,
+  ) {
+    return this.chatService.postThread(userId, course.id, threadData);
+  }
+
+  //POST chat/forums/courses/course:id/threads/thread:id - Send a message in a thread
+  @Post('forums/courses/:courseId/threads/:threadId')
+  @ApiParam({ name: 'courseId', required: true, description: 'Course ID' })
+  @ApiParam({ name: 'threadId', required: true, description: 'Thread ID' })
+  @ApiOperation({ summary: 'Send a message in a thread' })
+  @ApiResponse({ status: 201, description: 'Message sent successfully' })
+  @ApiResponse({ status: 404, description: 'Message not found' })
+  async sendMessageToThread(
+    @GetUser('userId') userId: string,
+    @ExistParam(
+      { idKey: 'courseId', modelName: 'Course' },
+      CheckExistValidatorPipe,
+    )
+    course: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'threadId', modelName: 'Thread' },
+      CheckExistValidatorPipe,
+    )
+    thread: {
+      id: string;
+      modelName: string;
+    },
+    @Body() threadMessageData: ThreadMessageDto,
+  ) {
+    return this.chatService.sendMessageToThread(
+      userId,
+      course.id,
+      thread.id,
+      threadMessageData,
+    );
+  }
+
+  //POST chat/forums/courses/course:id/threads/thread:id/messages/message:id - Reply to a Thread message
+  @Post('forums/courses/:courseId/threads/:threadId/messages/:messageId')
+  @ApiParam({ name: 'courseId', required: true, description: 'Course ID' })
+  @ApiParam({ name: 'threadId', required: true, description: 'Thread ID' })
+  @ApiParam({ name: 'messageId', required: true, description: 'Message ID' })
+  @ApiOperation({ summary: 'Reply to a thread message' })
+  @ApiResponse({ status: 201, description: 'Reply sent successfully' })
+  @ApiResponse({ status: 404, description: 'Reply not found' })
+  async replyToThreadMessage(
+    @GetUser('userId') userId: string,
+    @ExistParam(
+      { idKey: 'courseId', modelName: 'Course' },
+      CheckExistValidatorPipe,
+    )
+    course: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'threadId', modelName: 'Thread' },
+      CheckExistValidatorPipe,
+    )
+    thread: {
+      id: string;
+      modelName: string;
+    },
+    @ExistParam(
+      { idKey: 'messageId', modelName: 'Message' },
+      CheckExistValidatorPipe,
+    )
+    message: {
+      id: string;
+      modelName: string;
+    },
+    @Body() threadMessageReplyData: ThreadMessageReplyDto,
+  ) {
+    return this.chatService.replyToThreadMessage(
+      userId,
+      course.id,
+      thread.id,
+      message.id,
+      threadMessageReplyData,
+    );
+  }
 }
--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -2,10 +2,15 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Model, Schema, Types } from 'mongoose';
 import { RoomMessage } from 'src/database/schemas/roomMessage.schema';
 import { RoomMessageDto } from './dto/RoomMessageDto';
+import { ThreadDto } from './dto/ThreadDto';
+import { ThreadMessageDto } from './dto/ThreadMessageDto';
+import { ThreadMessageReplyDto } from './dto/ThreadMessageReplyDto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/database/schemas/user.schema';
 import { Course } from 'src/database/schemas/course.schema';
-import { Console } from 'console';
+import { ThreadMessage } from 'src/database/schemas/threadMessage.schema';
+import { ThreadMessageReply } from 'src/database/schemas/threadMessageReply.schema';
+import { Thread } from 'src/database/schemas/thread.schema';
 
 @Injectable()
 export class ChatService {
@@ -14,6 +19,11 @@ export class ChatService {
     private readonly roomMessageModel: Model<RoomMessage>,
     @InjectModel(User.name) private readonly userModel: Model<User>,
     @InjectModel(Course.name) private readonly courseModel: Model<Course>,
+    @InjectModel(Thread.name) private readonly threadModel: Model<Thread>,
+    @InjectModel(ThreadMessage.name)
+    private readonly threadMessageModel: Model<ThreadMessage>,
+    @InjectModel(ThreadMessageReply.name)
+    private readonly threadMessageReplyModel: Model<ThreadMessageReply>,
   ) {}
 
   async getRoomMessages(courseId: string) {
@@ -111,5 +121,141 @@ export class ChatService {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+  }
+
+  //---------------------------------- FORUMS ----------------------------------\\
+
+  async getCourseThreads(course_id: string) {
+    try {
+      const threads = await this.threadModel
+        .find({ course_id: new Types.ObjectId(course_id) })
+        .populate({
+          path: 'creator_id',
+          select: 'name role',
+        })
+        .sort({ createdAt: 1 });
+
+      if (!threads) {
+        return {
+          statusCode: HttpStatus.OK,
+          message: 'No threads found',
+          data: [],
+        };
+      }
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Threads fetched successfully',
+        data: threads,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        `Database error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async getThreadMessages(course_id: string, thread_id: string) {
+    try {
+      const messages = await this.threadMessageModel
+        .find({
+          course_id: new Types.ObjectId(course_id),
+          thread_id: new Types.ObjectId(thread_id),
+        })
+        .populate('sender_id', 'name role')
+        .sort({ createdAt: 1 });
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Messages fetched successfully',
+        data: messages,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        `Database error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async getThreadMessageReplies(
+    course_id: string,
+    thread_id: string,
+    message_id: string,
+  ) {
+    try {
+      const replies = await this.threadMessageReplyModel
+        .find({
+          course_id: new Types.ObjectId(course_id),
+          thread_id: new Types.ObjectId(thread_id),
+          parent_id: new Types.ObjectId(message_id),
+        })
+        .populate('sender_id', 'name role')
+        .sort({ createdAt: 1 });
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Replies fetched successfully',
+        data: replies,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        `Database error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async postThread(
+    creator_id: string,
+    course_id: string,
+    threadData: ThreadDto,
+  ) {
+    try {
+      const { title, description } = threadData;
+      const thread = new this.threadModel({
+        course_id: new Types.ObjectId(course_id),
+        creator_id: new Types.ObjectId(creator_id),
+        title,
+        description,
+      });
+
+      await this.threadModel.create(thread);
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Thread created successfully',
+        data: thread,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        `Database error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async sendMessageToThread(
+    userId: string,
+    id: string,
+    id1: string,
+    messageData: ThreadMessageDto,
+  ) {
+    return 1;
+  }
+  async replyToThreadMessage(
+    id: string,
+    id1: string,
+    id2: string,
+    id3: string,
+    messageData: ThreadMessageReplyDto,
+  ) {
+    return 1;
   }
 }
