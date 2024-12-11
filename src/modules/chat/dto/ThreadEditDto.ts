288a8676f1fa0a2a808c39a2ff5e9f601f4d8781
Powerscore <alaa.ashraf.helmy@gmail.com>
2024-12-11 22:50:23 +0200

    fix: ThreadEditDto nullable

--- src/modules/chat/dto/ThreadEditDto.ts
+++ src/modules/chat/dto/ThreadEditDto.ts
@@ -1,19 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class ThreadEditDto {
   @ApiProperty({
     description: 'The title of the thread',
     example: "Today's task",
   })
+  @IsOptional()
   @IsString()
-  title: string;
+  title?: string;
 
   @ApiProperty({
     description: 'The description of the thread',
     example:
       "Hello, what is todays task's solution? I have tried but I am stuck.",
   })
+  @IsOptional()
   @IsString()
-  description: string;
+  description?: string;
 }
