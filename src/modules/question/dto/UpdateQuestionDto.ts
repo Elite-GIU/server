64fb2d69ff295a82bf63cb52c7933dd74bc40ed5
Powerscore <alaa.ashraf.helmy@gmail.com>
2024-12-21 19:57:38 +0200

    fix: update question

--- src/modules/question/dto/UpdateQuestionDto.ts
+++ src/modules/question/dto/UpdateQuestionDto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateQuestionDto } from './CreateQuestionDto';
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
+export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
+
+    question?: string;
+
+    choices?: string[];
+
+    right_choice?: string;
+
+    difficulty?: number;
+
+    type?: string;
+
+}
